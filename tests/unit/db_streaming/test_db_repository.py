c8703289b0893e0d501fa451a442eae94d12c90f
Ian888888 <ianwebikirda@gmail.com>
2026-07-13 12:34:50 +0700

    db_streaming

---
+++ tests/unit/db_streaming/test_db_repository.py
@@ -0,0 +1,55 @@
+import pytest 
+from src.db_streaming.db_streaming_repository import DbStreamingRepository
+
+class TestDbStreamingRepository:
+    def test_db_streaming_successed_etl_cycle(self):
+        """
+        Menguji aliran penuh .execute_sync() pada modul DB Streaming.
+        Memastikan sirkuit utama mengembalikan True dari hulu ke hilir.
+        """
+        repo = DbStreamingRepository(connection="postgresql://localhost:5432/prod_db")
+        status = repo.execute_sync()
+        assert status is True
+    
+    def test_transformationn_logic_clean_and_masking_data(self):
+        """
+        Menguji akurasi pembersihan teks dan penyamaran data sensitif (UU PDP):
+        - Status dengan spasi gaib dan huruf kecil harus bersih & kapital ("pending " -> "PENDING")
+        - Kolom email harus tersamar dengan benar ("ardian.webi@gmail.com" -> "a***********@gmail.com")
+        """
+        repo = DbStreamingRepository(connection="postgresql://dummy_url")
+        # Data tiruan (Dummy) untuk uji logika
+        raw_data_dummy = [
+            {
+                "order_id": 88,
+                "amount": 8000.0,
+                "status": " failed ",
+                "costumer_email": "ardian.webi@gmail.com"
+            }
+        ]
+
+        # Hasil yang kita harapkan secara presisi
+        ekspektasi_hasil = [
+            {
+                "order_id":88,
+                "amount": 8000.0,
+                "status": "FAILED",                # Harus bersih dari spasi & kapital
+                "customer_email": "i******@example.com"  # Karakter ke-2 dan seterusnya diganti bintang
+            }
+        ]
+
+        hasil_aktual = repo.trsf_clean_data(raw_data_dummy)
+        # Memastikan output transformasi 100% cocok dengan ekspektasi bisnis
+        assert hasil_aktual == ekspektasi_hasil
+    
+    def test_helper_masking_email(self):
+        """
+        Menguji ketahanan fungsi private _apply_email_masking jika menerima
+        input string email yang tidak valid atau rusak dari database.
+        """
+        repo = DbStreamingRepository(connection_string="postgresql://dummy_url")
+        email_rusak = "bukan_email_valid_tanpa_at"
+        ekspektasi_fallback = "masked_user@hidden.com"
+        # Memanggil fungsi private menggunakan nama aslinya
+        hasil_masking = repo._apply_email_masking(email_rusak)
+        assert hasil_masking == ekspektasi_fallback
